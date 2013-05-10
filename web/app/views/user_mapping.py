b4ab1725d6aec27727c2973f614f70b5e988fe1e
terraformer <cinjon@alum.mit.edu>
2013-05-10 17:22:38 -0400

    user_mapping path explication

--- web/app/views/user_mapping.py
+++ web/app/views/user_mapping.py
@@ -1,7 +1,5 @@
+import app
 import json
-from app import flask_app, utility
-from app.models import user, mapping
-from app.views.index import go_to_index
 from flask import g, render_template, url_for, redirect
 from flask.ext.login import login_required
 
@@ -10,27 +8,27 @@ show_canvas = True
 #display a saved version at /user/<uname>/<mname>
 #display an edit version at /user/<uname>/edit/<mname>
 
-@flask_app.route('/user/<uname>/<mname>', methods=['GET'])
+@app.flask_app.route('/user/<uname>/<mname>', methods=['GET'])
 def user_mapping(uname, mname):
-    u = user.user_with_name(uname)
+    u = app.models.user.user_with_name(uname)
     if not u:
         flash('No user with name %s, should have 404ed' % uname)
-        return go_to_index()
+        return app.views.index.go_to_index()
 
-    m = mapping.mapping_with_userid_and_name(u.id, mname)
+    m = app.models.mapping.mapping_with_userid_and_name(u.id, mname)
     if not m:
         flash('No mapping with name %s, should have 404ed' % mname)
         return redirect(url_for('user_profile', name=uname))
 
-    vis = mapping.visualize(m)
+    vis = app.models.mapping.visualize(m)
     return render_template('user_mapping.html', user=u, mapping=json.dumps(vis), mname=json.dumps(m.name), show_canvas=json.dumps(show_canvas))
 
-@flask_app.route('/user/<uname>/edit/<mname>', methods=['GET'])
+@app.flask_app.route('/user/<uname>/edit/<mname>', methods=['GET'])
 # @login_required
 def user_mapping_edit(uname, mname):
 #     if g.user.name != uname:
 #         return redirect(url_for('user_mapping', uname=uname, mname=mname))
-    vis = mapping.test_visualize("test mapping")
+    vis = app.models.mapping.test_visualize("test mapping")
     return render_template('user_mapping.html', mapping=json.dumps(vis), show_canvas=json.dumps(True))
 
 
