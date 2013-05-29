04e296a7d6324e0f72599ed8902fef4b3d80e03d
terraformer <cinjon@alum.mit.edu>
2013-05-29 03:08:31 -0400

    changes to user_mapping, getting the map editor in place.

--- web/app/models/mapping.py
+++ web/app/models/mapping.py
@@ -1,28 +1,28 @@
-from app import db
-from app import utility
-import sql
-import note
+import app
+import math
 from sqlalchemy import and_
 
-class Mapping(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    creation_time = db.Column(db.DateTime)
-    name = db.Column(db.String(120))
-    kaizen_user_id = db.Column(db.Integer, db.ForeignKey('kaizen_user.id'), index=True)
-    binding = db.Column(db.SmallInteger)
-    notes = db.relationship('Note', backref='mapping', lazy='dynamic')
+start_visualization_from_blank = True
+
+class Mapping(app.db.Model):
+    id = app.db.Column(app.db.Integer, primary_key=True)
+    creation_time = app.db.Column(app.db.DateTime)
+    name = app.db.Column(app.db.String(120))
+    kaizen_user_id = app.db.Column(app.db.Integer, app.db.ForeignKey('kaizen_user.id'), index=True)
+    binding = app.db.Column(app.db.SmallInteger)
+    notes = app.db.relationship('Note', backref='mapping', lazy='dynamic')
 
     def set_binding(self, binding):
         self.binding = binding
-        sql.commit()
+        app.models.sql.commit()
 
     def add_note(self, data, keyCode):
-        text = utility.decodeJS(data['text'])
-        title = utility.decodeJS(data['title'])
-        href = utility.decodeJS(data['href'])
+        text = app.utility.decodeJS(data['text'])
+        title = app.utility.decodeJS(data['title'])
+        href = app.utility.decodeJS(data['href'])
         if 'ugn' in data: #TODO: userGeneratedNote
             pass
-        note.create_note(text, title, href, self.id, keyCode)
+        app.models.note.create_note(text, title, href, self.id, keyCode)
 
     def get_all_notes(self):
         return [n for n in self.notes]
@@ -35,14 +35,14 @@ class Mapping(db.Model):
         self.name = name
         self.kaizen_user_id = kaizen_user_id
         self.binding = binding
-        self.creation_time = utility.get_time()
+        self.creation_time = app.utility.get_time()
 
     def __repr__(self):
         return self.name
 
 def create_mapping(kaizen_user_id, name, binding=-1):
     mapping = Mapping(name=name, binding=binding, kaizen_user_id=kaizen_user_id)
-    sql.add(mapping)
+    app.models.sql.add(mapping)
     return mapping
 
 def json_mappings(maps):
@@ -60,27 +60,66 @@ def mapping_with_userid_and_name(uid, name):
     return None
 
 def visualize(m):
-    if len(m.notes.all()) == 0:
-        return None
+    if start_visualization_from_blank:
+        return visualize_from_null(m)
+    else:
+        return visualize_from_save(m)
 
-    urls = {}
-    for n in m.notes:
-        s = n.site
-        if s.url not in urls:
-            urls[s.url] = {'title':s.title,
-                           'start_time':utility.get_unixtime(n.creation_time)}
-        urls[s.url].setdefault('notes', []).append(
-            {'time':utility.get_unixtime(n.creation_time), 'text':n.text})
+def visualize_from_null(m):
+    line_weight = 5 #1-10 (noninteger fine)
+    node_weight = 5 #1-10 (noninteger fine)
+    width = 1050
+    height = 600
+    root_distance = max(width/5, height/5)
+    root_position = (width/2, height/2)
+    root_radius   = max(node_weight*6, min(math.sqrt(width), math.sqrt(height)))
+    note_distance = root_distance / 3
+    note_radius   = root_radius * 2 / 5
+    vis = {'mapname':m.name, 'root':_make_node(root_position, root_radius)}
 
-    time_sorted_urls = sorted(urls.iteritems(), key=lambda (k,v): v['start_time'])
-    latest_time = time_sorted_urls[-1][1]['start_time']
+    sites = m.get_all_sites()
+    if len(sites) == 0:
+        return visualization
+    else:
+        vis['site_links'] = {} #maintain this in alphabetical ordering
+        vis['note_links'] = {}
 
-    ret = []
-    for url in time_sorted_urls:
-        ret.append({'url':url[0], 'title':url[1]['title'], 'notes':url[1]['notes'],
-                    'radii':(latest_time - url[1]['start_time']) + 60})
-    return ret
+    angle_s = 2 * math.pi / len(sites)
+    for i, site in enumerate(sites):
+        angle_spos = i * angle_s
+        scx = root_position[0] + math.cos(angle_spos) * (root_radius + root_distance)
+        scy = root_position[1] + math.sin(angle_spos) * (root_radius + root_distance)
+        s_id = 'site_id_' + str(i)
+        vis[s_id] = _make_node((scx, scy), root_radius, url=site.url, title=site.title)
+        vis['site_links']['site_id_root' + '   ' + s_id] = line_weight
 
+        notes = site.notes.all()
+        if len(notes) > 0:
+            angle_n = 2 * angle_s / len(notes)
+            for j, note in enumerate(notes):
+                angle_counter = 0
+                angle_npos = angle_spos + angle_n * j - angle_s/2
+                ncx = -1
+                ncy = -1
+                while (ncx < 0 or ncx > width or ncy < 0 or ncy > height):
+                    ncx = scx + math.cos(angle_npos - angle_spos * angle_counter) * (root_radius + note_distance)
+                    ncy = scy + math.sin(angle_npos - angle_spos * angle_counter) * (root_radius + note_distance)
+                    angle_counter += 1
+                n_id = 'note_id_' + str(i) + '_' + str(j)
+                vis[n_id] = _make_node((ncx, ncy), note_radius, text=note.text, site=i,
+                                       time=app.utility.get_unixtime(note.creation_time))
+                vis['note_links'][s_id + '   ' + n_id] = line_weight
+    return vis
+
+def visualize_from_save(m):
+    #TODO
+    return {}
+
+def _make_node(position, radius, **kwargs):
+    node = {'position':position, 'radius':radius}
+    for key, value in kwargs.items():
+        node[key] = value
+    return node
 def test_visualize(mapname):
     #time should be real time as well. That needs to be displayed.
     return {"sites":[{"notes":[{"text":"qclub and shit lot of costs, etc", "time":100},
--- web/app/static/js/user_mapping.js
+++ web/app/static/js/user_mapping.js
@@ -1,194 +1,283 @@
-var width, height, radius, linklength, groups;
+// mapdata:
+// {mapname:name,
+//  site_links:
+//     {(site_id_i + '   ' + site_id_j):num_strength,...} //Includes root
+//  note_links:
+//     {(site_id_i + '   ' + note_id_j):num_strength,...} //Includes root
+//  site_id_root:
+//     {position:(cx, cy),
+//      radius:num_strength}
+//  site_id_0:
+//     {position:(cx, cy),
+//      radius:num_strength,
+//      url:url,
+//      title:title}
+//  ...
+//  note_id_0:
+//     {position:(cx, cy),
+//      radius:num_strength,
+//      text:text,
+//      time:#,
+//      site:{0, 1, ...}}
+//  ...
+// }
 
-function showMappingVisualization(mapdata, show) {
-    if (mapdata && show) {
-        $('#mapName').html('<p><b>' + mapdata.mapname + '</b></p>');
-        start_map_nav();
-        start_map_sandlot(mapdata);
-        start_map_sites(mapdata);
-        start_map_notes(mapdata);
-    } else {
-        console.log('no notes, yo');
+
+var width, height, radius;
+var links, groups, rects;
+var mapdata;
+//Groups are a draggable selection, e.g. a node along with its txt and notes
+//Links are a deletable line. When dragging a group, should drag the link with it
+//Rects is a dict of note_id to rectangle.
+
+function showMappingVisualization(server_data, show) {
+    if (server_data && show) {
+        mapdata = server_data;
+        start_map_sandlot();
     }
 }
 
-start_map_nav = function() {
-    //map navigation, e.g. "add", "delete", "connect"
-}
-
-start_map_sandlot = function(mapdata) {
+start_map_sandlot = function() {
     set_initial_conditions($("#map-sandlot"));
     var svg = d3.select("#map-sandlot")
         .append("svg:svg")
         .style("width", width)
         .style("height", height);
 
-    if ("start_state" in mapdata) {
-        //Do start_state stuff ...
-    } else {
-        start_map_sandlot_from_blank(mapdata, svg);
+    for (var key in mapdata) {
+        if (key == 'site_links' || key == 'note_links' || key.slice(0, 8) == 'note_id_') {
+            continue;
+        }
+
+        position = mapdata[key]['position']
+        radius = mapdata[key]['radius']
+        if (key == 'root') {
+            make_root(svg, mapdata.mapname, position, radius);
+        } else if (key.slice(0, 4) == 'site') {
+            make_site(svg, key.slice(8), position, radius);
+        }
     }
+
+    //We want sites to exist before notes so that we can group them together.
+    for (var key in mapdata) {
+        if (key.slice(0,8) == 'note_id_') {
+            make_note(svg, key.slice(8), mapdata[key]['position'],
+                      mapdata[key]['radius'], mapdata[key]['site']);
+        } else {
+            continue
+        }
+    }
+
+    make_site_links(svg, mapdata.site_links);
+    make_note_links(svg, mapdata.note_links);
 };
-
-start_map_sites = function(mapdata) {
-    console.log(' in start map sites');
-    console.log(mapdata);
-    var div_id, site_count, node_r, div_width, div_height, _i;
-    div_id = '#map-sites';
-    div_width = $(div_id).width();
-    div_height = $(div_id).height();
-    site_count = mapdata.sites.length;
-    node_r = Math.min(div_height/2 - 10, div_width / site_count - 10);
-    var svg = d3.select(div_id)
-        .append("svg:svg")
-        .style("width", div_width)
-        .style("height", div_height);
-
-    for (_i = 0; _i < site_count; _i++) {
-        var g = svg.append("svg:g");
-        var node = g.append("svg:circle")
-            .attr("r", node_r)
-            .attr("cx", node_r*(2*_i + 1) + 10*(_i + 1))
-            .attr("cy", div_height / 2)
-            .attr("fill", "#dceaf4");
-        var text = make_node_text(g, "testing", node);
-    }
-}
-
-start_map_notes = function(mapdata) {
-    //TODO
-}
 
 set_initial_conditions = function(sandlot) {
     set_dimensions = function(sandlot) {
-        width = sandlot.width();
-        height = 500;
-    };
-    set_default_radius = function() {
-        radius = Math.max(30, Math.min(Math.sqrt(width), Math.sqrt(height)))
-    };
-    set_default_linklength = function() {
-        linklength = 3 * radius;
+        width = 1050; //width = sandlot.width();
+
+        height = 600;
     };
     set_groups = function() {
         groups = {}; //Keyed by node_id
     };
+    set_links = function() {
+        links = {}; //{g_id_i:[(g_id_j, c1), (g_id_k, c2),...] --> c1/c2 is whether it's first or second set of coords
+    }
+    set_rects = function() {
+        rects = {};
+    }
 
     set_dimensions(sandlot);
-    set_default_radius();
-    set_default_linklength();
     set_groups();
+    set_links();
+    set_rects();
 };
 
-start_map_sandlot_from_blank = function(mapdata, svg) {
-    var rootG = make_node(svg, "root", mapdata.mapname);
-//     add_links_to_node(rootG, 5, svg);
-};
+function display_note(name) {
+    data = mapdata['note_id_' + name];
+    var date = new Date(data.time * 1000);
+    $("#note-time")[0].innerHTML = '<p>' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '</p>';
+    $("#note-text")[0].innerHTML = '<p>' + data.text + '</p>';
+}
 
 var drag = d3.behavior.drag().on("drag", move);
 function move() {
     var circle = d3.select(this);
-    var group = groups[this.parentNode.id];
-    var text = group.select('text');
+    var g = $("#" + this.parentNode.id)[0];
+    var texts = g.getElementsByTagName('text');
+    var rectangles = g.getElementsByTagName('rect');
 
     var cx = parseInt(circle.attr("cx"));
     var cy = parseInt(circle.attr("cy"));
     var r  = parseInt(circle.attr("r"));
-    var box_width =  Math.max(text.node().getBBox().width, 2*r);
+    var box_width =  Math.max(g.getElementsByClassName('siteText')[0].getBBox().width, 2*r);
     var new_cx = Math.max(r, Math.min(width - box_width + r, d3.event.dx + cx));
     var new_cy = Math.max(r, Math.min(height - r, d3.event.dy + cy));
+
     circle
         .attr("cx", new_cx)
         .attr("cy", new_cy);
-    text
-        .attr("x", new_cx - circle.attr("r"))
-        .attr("y", new_cy);
+
+//     texts.each(function(text) {
+//         console.log('text: ' + text);
+//         var new_x = text.attr("x") - (new_cx - cx);
+//         var new_y = text.attr("y") - (new_cy - cy);
+//         text
+//             .attr("x", new_x)
+//             .attr("y", new_y);
+//     });
+    for (var rect in rectangles) {
+        var new_x = rect.attr("x") - (new_cx - cx);
+        var new_y = rect.attr("y") - (new_cy - cy);
+        rect
+            .attr("x", new_x)
+            .attr("y", new_y);
+    }
 };
 
-make_node = function(svg, node_id, name) {
-    var circle, text, g;
+make_root = function(svg, name, position, radius) {
+    var circle, text, g, _id;
+    _id = 'g_site_root';
     g = svg.append("svg:g")
-        .attr("id", "g_" + node_id);
-    groups['g_' + node_id] = g
-    if (node_id == "root") {
-        circle = g.append("svg:circle")
-            .attr("class", "node")
-            .attr("cx", width / 2)
-            .attr("cy", height / 2)
-            .attr("r", radius)
-            .attr("fill", "wheat")
-            .attr("stroke", "#dceaf4")
-            .call(drag);
-    } else {
-
-    }
-
-    text = make_node_text(g, name, circle);
-    return g;
+        .attr("id", _id);
+    groups[_id] = g
+    circle = g.append("svg:circle")
+        .attr("class", "node")
+        .attr("cx", position[0])
+        .attr("cy", position[1])
+        .attr("r", radius)
+        .attr("fill", "wheat")
+        .attr("stroke", "#dceaf4")
+        .call(drag);
+    text = make_node_text(g, name, position[0] - radius, position[1], 'rootText');
 }
 
-make_node_text = function(g, name, node) {
+make_site = function(svg, name, position, radius) {
+    var circle, text, g, _id;
+    _id = 'g_site_' + name;
+    g = svg.append("svg:g")
+        .attr("id", _id);
+    groups[_id] = g;
+    circle = g.append("svg:circle")
+        .attr("class", "node")
+        .attr("r", radius)
+        .attr("cx", position[0])
+        .attr("cy", position[1])
+        .attr("fill", "#dceaf4")
+        .call(drag);
+    text = make_node_text(g, name, position[0] - radius, position[1], 'siteText');
+}
+
+make_node_text = function(g, name, x, y, text_class) {
     return g.append("text")
-        .attr("x", node.attr("cx") - node.attr("r"))
-        .attr("y", node.attr("cy"))
+        .attr("x", x)
+        .attr("y", y)
         .text(name)
         .attr("font-family", "helvetica")
         .attr("font-size", "16px")
         .attr("fill", "red")
-        .attr("class", "nodeText");
+        .attr("class", text_class);
 };
 
-add_links_to_node = function(node, number, svg) {
-    var angle, node_x1, node_y1, node_r;
-    angle = 2 * Math.PI / number;
-    node_r = parseInt(node.attr("r"));
-    for (var _i = 0; _i < number; _i++) {
-        var x1, y1, x2, y2, rim_angle;
-        rim_angle = angle*_i;
-        x1 = parseInt(node.attr("cx")) + Math.cos(rim_angle) * node_r;
-        y1 = parseInt(node.attr("cy")) + Math.sin(rim_angle) * node_r;
-        x2 = x1 + Math.cos(rim_angle) * linklength;
-        y2 = y1 + Math.sin(rim_angle) * linklength;
-        var link = svg.append("svg:line")
-            .attr("x1", x1)
-            .attr("y1", y1)
-            .attr("x2", x2)
-            .attr("y2", y2)
-            .attr("class", "link")
-            .attr("class", node.attr("id"))
-            .style("stroke", "#CCC");
+make_note = function(svg, name, position, radius, site) {
+    var rectangle, side;
+    var g = groups['g_site_' + site];
+    groups['g_note_' + name] = g;
+
+    side = 30;
+    rectangle = g.append("svg:rect")
+        .attr("class", "note")
+        .attr("x", position[0] - side/2)
+        .attr("y", position[1] - side/2)
+        .attr("width", side)
+        .attr("height", side)
+        .attr("fill", "#FF7340")
+        .attr("rx", 7)
+        .attr("ry", 7)
+        .attr("stroke-width", 5)
+        .attr("opacity", .5)
+        .attr("id", name)
+        .on("click", function() {
+            display_note(name);});
+    make_node_text(g, name, position[0] - side/2, position[1], 'noteText');
+    rects['r_note_' + name] = rectangle;
+}
+
+make_site_links = function(svg, site_links) {
+    for (var key in site_links) {
+        var line_weight = site_links[key];
+        var link_pair = key.split('   ');
+        var id1 = link_pair[0].slice(8);
+        var id2 = link_pair[1].slice(8);
+        var g1 = groups['g_site_' + id1];
+        var g2 = groups['g_site_' + id2];
+        var link = make_site_link(svg, g1.select('circle'), g2.select('circle'),
+                                  line_weight, 'groups_s_' + id1 + '_s_' + id2);
+        add_link_to_links(link, g1, g2);
     }
-};
+}
 
-start_sidebar_with_notes = function(url_notes, mapping, sidebar_id) {
-    var sidebar = document.getElementById(sidebar_id);
-    for (var i = 0; i < url_notes.length; i++) {
-        var url = url_notes[i];
-        var title = document.createElement('div');
-        title.class = 'row-fluid';
-        title.innerHTML = '<a href="' + url.url + '"><p>' + url.title + '</p></a>';
-        sidebar.appendChild(title);
+make_site_link = function(svg, c1, c2, weight, end_ids) {
+    //TODO: utilize weight
+    var link = svg.append("svg:line")
+        .attr("x1", c1.attr("cx"))
+        .attr("y1", c1.attr("cy"))
+        .attr("x2", c2.attr("cx"))
+        .attr("y2", c2.attr("cy"))
+        .attr("class", "site_link")
+        .attr("id", end_ids)
+        .style("stroke", "#CCC");
+    return link;
+}
 
-        for (var j = 0; j < url.notes.length; j++) {
-            var rowfluid = document.createElement('div');
-            rowfluid.class = 'row-fluid';
-            var spanOne = document.createElement('div');
-            spanOne.class = 'span1';
-            var spanTen = document.createElement('div');
-            spanTen.class = 'span10 offset1';
-            rowfluid.appendChild(spanOne);
-            rowfluid.appendChild(spanTen);
-            title.appendChild(rowfluid);
-
-            var note = url.notes[j];
-            var date = new Date(note.time * 1000);
-//             var textBox = fitString(null, note.text, 50);
-            var text = note.text;
-            spanOne.innerHTML = '<p>' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '</p>';
-            spanTen.innerHTML = '<p>' + text + '</p>';
-        }
+make_note_links = function(svg, note_links) {
+    for (var key in note_links) {
+        var line_weight = note_links[key];
+        var link_pair = key.split('   ');
+        var site_id = link_pair[0].slice(8);
+        var note_id = link_pair[1].slice(8);
+        var g1 = groups['g_site_' + site_id];
+        var g2 = groups['g_note_' + note_id];
+        var r  = rects['r_note_' + note_id];
+        var link = make_note_link(svg, g1.select('circle'), r, line_weight,
+                                  'groups_s_' + site_id + '_n_' + note_id);
+        add_link_to_links(link, g1, g2);
     }
-};
+}
+
+make_note_link = function(svg, c, r, weight, end_ids) {
+    //TODO: utilize weight
+    var link = svg.append("svg:line")
+        .attr("x1", parseInt(c.attr("cx")))
+        .attr("y1", parseInt(c.attr("cy")))
+        .attr("x2", parseInt(r.attr("x")) + parseInt(r.attr("width")/2))
+        .attr("y2", parseInt(r.attr("y")) + parseInt(r.attr("height")/2))
+        .attr("class", "note_link")
+        .attr("id", end_ids)
+        .style("stroke", "#CCC");
+    return link;
+}
+
+add_link_to_links = function(link, g1, g2) {
+    id1 = g1.attr("id");
+    id2 = g2.attr("id");
+    if (!(id1 in links)) {
+        links[id1] = [];
+    }
+    if (!(id2 in links)) {
+        links[id2] = [];
+    }
+
+    links[id1].push((link, '1'));
+    links[id2].push((link, '2'));
+}
+
+// reorder_edges_below = function(svg) {
+//     gs = svg.selectAll("g");
+//     for (var g in gs) {
+//         ;
+
 
 // fitString = function(ctx, str, widthMax) {
 //     var textBlock = [];
