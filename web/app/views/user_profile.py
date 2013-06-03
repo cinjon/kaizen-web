80aba32496953c935a88574efaf0001a9155f215
PoliteSM <aholdtman@gmail.com>
2013-06-02 21:00:27 -0400

    some front end work

--- web/app/views/user_profile.py
+++ web/app/views/user_profile.py
@@ -1,5 +1,5 @@
 from app import flask_app
-from app.models import kaizen_user
+from app.models import kaizen_user, mapping
 from flask import g, render_template, redirect, flash, url_for
 from flask.ext.login import login_required
 
@@ -22,14 +22,24 @@ def user_profile(name):
     flash('No user with name %s, should have 404ed' % name)
     #TODO return no user with name 404 page
 
+# def format_datetime(value, format='medium'):
+#     if format == 'full':
+#         format="EEEE, d. MMMM y 'at' HH:mm"
+#     elif format == 'medium':
+#         format="EE dd.MM.y HH:mm"
+#     return babel.format_datetime(value, format)
+#
+# flask_app.jinja_env.filters['datetimeformat'] = format_datetime
+
 
 def view_mappings(u):
     mappings = []
+    # for m in u.get_current_mappings(mapping.Mapping.creation_time):
     for m in u.get_all_mappings_in_name_order():
         if m.binding > -1:
-            mappings.append({'name':str(m.name), 'binding':m.binding, 'notes':len(m.get_all_notes())})
+            mappings.append({'name':str(m.name), 'binding':m.binding, 'notes':m.get_all_notes()})
         else:
-            mappings.append({'name':str(m.name), 'binding':'-', 'notes':len(m.get_all_notes())})
+            mappings.append({'name':str(m.name), 'binding':'-', 'notes':m.get_all_notes()})
     return mappings
 
 
