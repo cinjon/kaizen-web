b8e79938a4a881ba2a72fd0bee5ee87ff16917f2
terraformer <cinjon@alum.mit.edu>
2013-06-03 23:46:33 -0400

    adjusted the user_mappings to be unfortunately duplicated until alex can combine

---
+++ web/app/static/js/user_mapping_alex.js
@@ -0,0 +1,26 @@
+$(function() {
+    $('a.removeNote').bind('click', function() {
+        $.getJSON('{{ url_for('remove_index') }}', {
+            loopId: $(this).closest('.controls-wrapper').children('.loopId').val(),
+            mapId: $(this).closest('.controls-wrapper').children('.mapId').val()
+        }, function(data) {
+            window.console.log(data.result);
+        });
+        return false;
+    });
+});
+
+$(function() {
+    $('input.reindexNote').keyup(function(event){
+        if(event.keyCode == 13){
+            $.getJSON('{{ url_for('reindex_note') }}', {
+                loopId: $(this).closest('.controls-wrapper').children('.loopId').val(),
+                targetIndex: $(this).val(),
+                mapId: $(this).closest('.controls-wrapper').children('.mapId').val()
+            }, function(data) {
+                window.console.log(data.result);
+            });
+        }
+        return false;
+    });
+});
