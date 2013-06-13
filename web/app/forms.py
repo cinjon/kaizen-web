d458296b62689189dad7590186de62b50ab6cb42
PoliteSM <aholdtman@gmail.com>
2013-06-13 00:28:57 -0400

    adding roles to kaizen_user init, and cleaning up registration form to not require first and last name

--- web/app/forms.py
+++ web/app/forms.py
@@ -2,5 +2,4 @@ from flask.ext.wtf import Form, TextField, SubmitField, Required, PasswordField
 from flask_security.forms import RegisterForm
 
 class ExtendedRegisterForm(RegisterForm):
-    first = TextField('first', validators=[Required()])
-    last = TextField('last', validators=[Required()])
+    name = TextField('name', validators=[Required()])
