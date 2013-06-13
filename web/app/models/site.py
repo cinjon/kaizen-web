ca61ddc63dc85dd21b1e628acf24c773b4497ed5
terraformer <cinjon@alum.mit.edu>
2013-06-12 21:53:41 -0400

    changes to old models

--- web/app/models/site.py
+++ web/app/models/site.py
@@ -7,7 +7,15 @@ class Site(app.db.Model):
     creation_time = app.db.Column(app.db.DateTime, index=True)
     title = app.db.Column(app.db.Text)
     notes = app.db.relationship('Note', backref='site', lazy='dynamic')
-    #TODO: Add another table that has a user-generated nickname for sites
+    deleted = app.db.Column(app.db.Boolean)
+    name = app.db.Column(app.db.String(120))
+
+    def __init__(self, url, title, name=None, deleted=False):
+        self.url = url
+        self.title = title
+        self.name = name
+        self.creation_time = app.utility.get_time()
+        self.deleted = deleted
 
     def update_title(self, title):
         self.title = title
@@ -16,12 +24,8 @@ class Site(app.db.Model):
         return {'sid'           : self.id,
                 'url'           : self.url,
                 'title'         : self.title,
+                'name'          : self.name,
                 'creation_time' : app.utility.serialize_datetime(self.creation_time)}
-
-    def __init__(self, url, title):
-        self.url = url
-        self.title = title
-        self.creation_time = app.utility.get_time()
 
 def create_site(url, title):
     site = Site(url, title)
