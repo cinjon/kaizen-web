f8f33003e48f76b62338b58d904a8331c9f668ed
terraformer <cinjon@alum.mit.edu>
2013-06-12 21:53:29 -0400

    new models for visualizing

--- web/app/models/node.py
+++ web/app/models/node.py
@@ -1,21 +1,30 @@
 import app
 
-NodeType = app.utility.enum(NOTE=1, SITE=2, ROOT=3)
+NodeTypes = app.utility.enum(MAPPING=1, SITE=2, NOTE=3)
 
 class Node(app.db.Model):
     id = app.db.Column(app.db.Integer, primary_key=True)
     x = app.db.Column(app.db.Integer)
     y = app.db.Column(app.db.Integer)
-    ty = app.db.Column(app.db.SmallInteger)
     vid = app.db.Column(app.db.Integer, app.db.ForeignKey('visualization.id'), index=True)
+    radius = app.db.Column(app.db.Integer)
+    node_type = app.db.Column(app.db.SmallInteger)
+    dom_id = app.db.Column(app.db.VARCHAR(6))
+    node_type_id = app.db.Column(app.db.Integer) #either a nid, a sid, or a mid
 
-    def __init__(self, vid, ty, x, y):
+    def __init__(self, vid, x, y, radius, node_type, node_type_id):
         self.x = x
         self.y = y
         self.vid = vid
-        self.ty = ty
+        self.radius = radius
+        self.dom_id = app.utility.generate_id() #random id for the dom, should be s.t. (vid, dom_id) is a unique pairing ... add that later
+        self.node_type = node_type
+        self.node_type_id = node_type_id
 
-def create_node(vid, ty, x, y):
-    node = Node(vid, ty, x, y)
+    def serialize(self):
+        return {'position':(self.x, self.y), 'radius':self.radius}
+
+def create_node(vid, x, y, radius, node_type, node_type_id):
+    node = Node(vid, x, y, radius, node_type, node_type_id)
     app.models.sql.add(node)
     return node
