99271d5c1337df0dc65fe9612b144fc545e023fb
cinjon <cinjon.resnick@gmail.com>
2013-12-03 13:51:15 -0800

    index; added back in the login forms

--- web/app/views/index.py
+++ web/app/views/index.py
@@ -1,28 +1,26 @@
 from app import flask_app
 from flask import g, render_template, redirect, url_for
-from app.forms import ExtendedRegisterForm
 from app.models import kaizen_user
 from flask_security.forms import LoginForm
+from app.forms import ExtendedRegisterForm
 
-@flask_app.route('/', methods=['GET'])
+@flask_app.route('/', methods=['GET', 'POST'])
 def index():
     if g.user:
         return user_index()
     else:
         return enter_index()
 
-def go_to_index():
-    return redirect(url_for('index'))
-
 def enter_index():
     return render_template('index.html',
                            register_user_form=ExtendedRegisterForm(),
-                           login_user_form=LoginForm(),
-                           title='Kaizen')
-
+                           login_user_form=LoginForm())
 
 def user_index():
     return redirect('/me')
+
+def go_to_index():
+    return redirect(url_for('index'))
 
 @flask_app.route('/google34d3fe92d155a2aa')
 @flask_app.route('/google34d3fe92d155a2aa.html')
