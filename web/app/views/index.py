5e054427bcaf38dd2eaaf3753e2b21d6c5f1a07b
cinjon <cinjon.resnick@gmail.com>
2013-12-03 11:12:24 -0800

    index: refactored logout/reg

--- web/app/views/index.py
+++ web/app/views/index.py
@@ -4,7 +4,6 @@ from app.forms import ExtendedRegisterForm
 from app.models import kaizen_user
 from flask_security.forms import LoginForm
 
-@flask_app.route('/index', endpoint='', methods=['GET'])
 @flask_app.route('/', methods=['GET'])
 def index():
     if g.user:
@@ -16,16 +15,14 @@ def go_to_index():
     return redirect(url_for('index'))
 
 def enter_index():
-    register_user_form = ExtendedRegisterForm()
-    login_user_form = LoginForm()
     return render_template('index.html',
-                           register_user_form=register_user_form,
-                           login_user_form=login_user_form,
-                           title='Kaizen. continuous improvement.')
+                           register_user_form=ExtendedRegisterForm(),
+                           login_user_form=LoginForm(),
+                           title='Kaizen')
 
 
 def user_index():
-    return redirect(url_for('user_profile_owner'))
+    return redirect('/me')
 
 @flask_app.route('/google34d3fe92d155a2aa')
 @flask_app.route('/google34d3fe92d155a2aa.html')
