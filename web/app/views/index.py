e1debe20923f58c1e97f149a59701f109a5c8abc
cinjon <cinjon.resnick@gmail.com>
2013-12-08 22:19:16 -0800

    howitworks / newfeatures: updated and reachable from index

--- web/app/views/index.py
+++ web/app/views/index.py
@@ -1,10 +1,10 @@
-from app import flask_app
+import app
 from flask import g, render_template, redirect, url_for
 from app.models import kaizen_user
 from flask_security.forms import LoginForm
 from app.forms import ExtendedRegisterForm
 
-@flask_app.route('/', methods=['GET', 'POST'])
+@app.flask_app.route('/', methods=['GET', 'POST'])
 def index():
     if g.user:
         return user_index()
@@ -22,7 +22,11 @@ def user_index():
 def go_to_index():
     return redirect(url_for('index'))
 
-@flask_app.route('/google34d3fe92d155a2aa')
-@flask_app.route('/google34d3fe92d155a2aa.html')
+@app.flask_app.route('/google34d3fe92d155a2aa')
+@app.flask_app.route('/google34d3fe92d155a2aa.html')
 def google_extension_verify():
     return render_template('google34d3fe92d155a2aa.html')
+
+@app.flask_app.route('/features')
+def new_features():
+    return render_template('new_features.html')
